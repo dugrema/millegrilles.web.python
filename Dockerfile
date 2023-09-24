c978fd467dd1ef09cb28eff74ffbe07f1ec22031
mathieu <mathieu.dugre@mdugre.info>
2023-09-24 19:02:52 -0400

    WIP build web

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM docker.maple.maceroc.com:5000/millegrilles_messages_python:2023.9.2
+
+ENV CERT_PEM=/run/secrets/cert.pem \
+    KEY_PEM=/run/secrets/key.pem \
+    CA_PEM=/run/secrets/pki.millegrille.cert \
+    MQ_HOSTNAME=mq \
+    MQ_PORT=5673 \
+    REDIS_HOSTNAME=redis \
+    REDIS_PASSWORD_PATH=/var/run/secrets/passwd.redis.txt \
+    WEB_PORT=1443
+
+EXPOSE 80 443
+
+# Creer repertoire app, copier fichiers
+COPY . $BUILD_FOLDER
+
+RUN cd $BUILD_FOLDER && \
+    python3 ./setup.py install
+
+CMD ["-m", "server", "--verbose"]
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import subprocess
+
+
+def get_version():
+    try:
+        with open('version.txt', 'r') as version_file:
+            version = version_file.readline().strip()
+    except FileNotFoundError:
+        with open('build.txt', "r") as buildno_file:
+            build_no = buildno_file.read().strip()
+
+        # commande_git_version = ['git', 'name-rev', '--name-only', 'HEAD']
+        commande_git_version = ['git', 'rev-parse', '--abbrev-ref', 'HEAD']
+        output_process = subprocess.run(commande_git_version, stdout=subprocess.PIPE)
+        version = output_process.stdout.decode('utf8').strip()
+        version = '%s.%s' % (version, build_no)
+        print("Version: %s" % (version))
+
+    return version
+
+setup(
+    name='millegrilles_web_python',
+    version='%s' % get_version(),
+    packages=find_packages(),
+    url='https://github.com/dugrema/millegrilles.web.python',
+    license='AFFERO',
+    author='Mathieu Dugre',
+    author_email='mathieu.dugre@mdugre.info',
+    description='Base web pour les applictions MilleGrilles avec client',
+    install_requires=[
+        'pytz>=2020.4',
+        'aiohttp>=3.8.1,<4',
+        'requests>=2.28.1,<3',
+        'pyjwt',
+        'python-socketio>=5.9,<6',
+        'aioredis',
+        'aiohttp-session[aioredis]'
+    ]
+)
