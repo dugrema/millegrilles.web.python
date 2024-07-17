2e498163de0ecc74914c8780b4938386c915158e
Mathieu Dugre <mathieu.dugre@mdugre.info>
2024-07-17 12:35:03 -0400

    Fix charger version pour branch master avec jenkins

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 from setuptools import setup, find_packages
 import subprocess
+import os
 
 
 def get_version():
-    try:
-        with open('version.txt', 'r') as version_file:
-            version = version_file.readline().strip()
-    except FileNotFoundError:
-        with open('build.txt', "r") as buildno_file:
-            build_no = buildno_file.read().strip()
 
-        # commande_git_version = ['git', 'name-rev', '--name-only', 'HEAD']
-        commande_git_version = ['git', 'rev-parse', '--abbrev-ref', 'HEAD']
-        output_process = subprocess.run(commande_git_version, stdout=subprocess.PIPE)
-        version = output_process.stdout.decode('utf8').strip()
-        version = '%s.%s' % (version, build_no)
-        print("Version: %s" % (version))
+    try:
+        # Utiliser variable d'environnement de build (jenkins)
+        version = os.environ['VBUILD']
+    except KeyError:
+        try:
+            with open('version.txt', 'r') as version_file:
+                version = version_file.readline().strip()
+        except FileNotFoundError:
+            with open('build.txt', "r") as buildno_file:
+                build_no = buildno_file.read().strip()
+
+            # commande_git_version = ['git', 'name-rev', '--name-only', 'HEAD']
+            commande_git_version = ['git', 'rev-parse', '--abbrev-ref', 'HEAD']
+            output_process = subprocess.run(commande_git_version, stdout=subprocess.PIPE)
+            version = output_process.stdout.decode('utf8').strip()
+            version = '%s.%s' % (version, build_no)
+            print("Version: %s" % (version))
 
     return version
 
